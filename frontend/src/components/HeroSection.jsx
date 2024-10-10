6e8b239dd8794a4ae209696fdfe48c0c6bc88b8f
tusharpatel01 <jskobri6@gmail.com>
2024-10-10 19:24:46 +0530

    commit all

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='text-center'>
+       <span className='px-8 py-3 rounded-full bg-gray-100 text-red-500 font-medium '>NO.1 jon hunt website
+       </span> 
+       <h1 className=' gap-20 text-5xl font-bold'>search ,apply & get your <span className='text-blue-700'>dream job</span></h1>
+    </div>
+  )
+}
+
+export default HeroSection
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
+      {/* <CategoryCarousel/>
+      <LatestJobs/>
+      <Footer/> */}
     </div>
   )
 }
